f81d0d89fb28fb2dc2bcf7998ff4753eb5d9600e
Sebastian Stemmer <stemmer.main@gmail.com>
2025-09-24 08:33:23 +0200

    main

--- solve/solve_test_script.py
+++ solve/solve_test_script.py
@@ -42,8 +42,9 @@ def find_trivial_solutions_test() -> None:
         at=None
     )
 
-    handled_trivial_solutions: SolutionPathNode = recursively_solve_trivial_solutions(
-        node=node
+    handled_trivial_solutions: tuple[SolutionPathNode | None, int] = recursively_solve_trivial_solutions(
+        node=node,
+        depth=0
     )
 
     trivial_solution_is_5_coord = Coord(
@@ -58,14 +59,14 @@ def find_trivial_solutions_test() -> None:
         entry_idx=get_entry_idx(row_idx=0, col_idx=7)
     )
 
-    assert handled_trivial_solutions.grid.cells[trivial_solution_is_5_coord].value == 5
-    assert handled_trivial_solutions.grid.cells[trivial_solution_is_8_coord].value == 8
+    assert handled_trivial_solutions[0].grid.cells[trivial_solution_is_5_coord].value == 5
+    assert handled_trivial_solutions[0].grid.cells[trivial_solution_is_8_coord].value == 8
 
-    assert handled_trivial_solutions.at is not None
-    assert handled_trivial_solutions.at.is_trivial
+    assert handled_trivial_solutions[0].at is not None
+    assert handled_trivial_solutions[0].at.is_trivial
 
-    assert handled_trivial_solutions.at.previous_node.at is not None
-    assert handled_trivial_solutions.at.previous_node.at.is_trivial
+    assert handled_trivial_solutions[0].at.previous_node.at is not None
+    assert handled_trivial_solutions[0].at.previous_node.at.is_trivial
 
 
 def solve_grid_test() -> None:
@@ -78,24 +79,21 @@ def solve_grid_test() -> None:
 
     solution_random: Grid = solve_grid(
         grid=grid,
-        guess_strategy=random_guess_strategy,
-        max_go_back_depth=None
+        guess_strategy=random_guess_strategy
     )
 
     assert is_equal(grid1=solution_random, grid2=str_to_grid(grid_as_str=solution_str))
 
     solution_ordered: Grid = solve_grid(
         grid=grid,
-        guess_strategy=ordered_guess_strategy,
-        max_go_back_depth=None
+        guess_strategy=ordered_guess_strategy
     )
 
     assert is_equal(grid1=solution_ordered, grid2=str_to_grid(grid_as_str=solution_str))
 
     solution_smallest_allowed: Grid = solve_grid(
         grid=grid,
-        guess_strategy=smallest_allowed_guess_strategy,
-        max_go_back_depth=None
+        guess_strategy=smallest_allowed_guess_strategy
     )
 
     assert is_equal(grid1=solution_smallest_allowed, grid2=str_to_grid(grid_as_str=solution_str))
@@ -103,17 +101,14 @@ def solve_grid_test() -> None:
 
 def create_filled_test() -> None:
     filled_by_random: Grid = create_filled(
-        max_go_back_depth=2,
         guess_strategy=random_guess_strategy
     )
 
     filled_by_ordered: Grid = create_filled(
-        max_go_back_depth=2,
         guess_strategy=ordered_guess_strategy
     )
 
     filled_by_smallest_allowed: Grid = create_filled(
-        max_go_back_depth=2,
         guess_strategy=smallest_allowed_guess_strategy
     )
 
